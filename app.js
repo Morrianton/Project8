1544c4359ab43514b30bf5ed8e96b5e8da37a289
Morrianton1985 <morrianton@gmail.com>
2018-04-04 10:11:14 -0600

    Added dependencies

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
+
