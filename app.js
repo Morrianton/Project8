96dca4ceffce57de80310b46dae334a4290a67d4
Morrianton1985 <morrianton@gmail.com>
2018-04-20 09:08:40 -0600

    removed form.pug created edit.pug other alterations

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 
@@ -11,6 +12,12 @@ let port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+let users = [];
+let id = 0;
+
 app.get('/', (req, res) => {
     res.render('index', {
         title: "Simple User Manager",
@@ -18,21 +25,34 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/users', (req, res) => {
-   res.render('users');
+app.post('/users', (req, res) => {
+    id++;
+    users.push({
+       userID: req.body.userID,
+       name: req.body.name,
+       age: req.body.age,
+       email: req.body.email,
+        id: id
+    });
+    res.render('users', {users: users});
 });
 
-app.post('/users', (req, res) => {
-    res.render('users', {
-        users: [
-            {firstName: req.body.firstName},
-            {lastName: req.body.lastName},
-            {age: req.body.age},
-            {email: req.body.email}
-        ]
+app.get('/index', (req, res) => {
+    res.render('index', {
+        title: "Simple User Manager",
+        pageTitle: "Simple User Manager"
     });
-    res.end();
 });
+
+app.get('/user/:id', (req, res) => {
+    res.render('edit', {
+        userID: req.body.userID,
+        name: req.body.name,
+        age: req.body.age,
+        id: req.params.id
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
